f9d95fd6ea63e2b55e25338a03aea967d4e317fc
Roberto Rodríguez <krowno.iso@gmail.com>
2022-05-06 10:18:24 +0200

    añadido redmine.py

--- redmine.py
+++ redmine.py
@@ -5,4 +5,7 @@ miclave = os.environ['key']
 payload = {'key' : miclave}
 
 r=requests.get(URL_BASE+"projects.json"+"?key=",params=payload)
-
+if r.status_code==200:
+    doc=r.json()
+    for p in doc["projects"]:
+        print(str(p["id"])+" "+p["name"])
